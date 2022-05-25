3b23303c60bb7f847e3a94dd61e5efaf63b40cc8
kmolski <starfighter413@gmail.com>
2022-05-25 09:54:09 +0200

    Use serde enum variant rename, refactor

--- src/main.rs
+++ src/main.rs
@@ -5,59 +5,70 @@ use std::error::Error;
 use std::io;
 
 #[derive(Serialize, Deserialize)]
+#[serde(rename_all = "lowercase")]
 enum MessageType {
-    resume,
-    pause,
-    stop
+    Resume,
+    Pause,
+    Stop,
 }
 
 #[derive(Serialize, Deserialize)]
 struct Message {
-    r#type: MessageType,
-    code: String
+    op: MessageType,
+    code: String,
+}
+
+const CODE_LENGTH: usize = 6;
+
+fn read_line() -> io::Result<String> {
+    let mut buf = String::new();
+    io::stdin().read_line(&mut buf)?;
+    Ok(buf.trim().to_string())
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
     let argv: Vec<String> = args().collect();
     let uri = &argv[1];
 
-    let mut connection = Connection::insecure_open(&uri)?;
+    let mut connection = Connection::insecure_open(uri)?;
     let channel = connection.open_channel(None)?;
     let exchange = Exchange::direct(&channel);
 
     let mut code = String::new();
-    let mut buf = String::new();
 
     loop {
         if !code.is_empty() {
             println!("[R]esume, [P]ause, [S]top or [Q]uit?");
-            buf.clear();
-            io::stdin().read_line(&mut buf)?;
-            buf = buf.trim().to_lowercase().to_string();
 
-            let r#type = match buf.chars().next().unwrap() {
-                'r' => MessageType::resume,
-                'p' => MessageType::pause,
-                's' => MessageType::stop,
-                'q' => { break; }
-                _   => { continue; }
+            let selection = read_line()?;
+            let op = match selection.as_str() {
+                "r" => MessageType::Resume,
+                "p" => MessageType::Pause,
+                "s" => MessageType::Stop,
+                "q" => {
+                    break;
+                }
+                _ => {
+                    continue;
+                }
             };
 
-            let message = Message{r#type, code: code.clone()};
+            let message = Message {
+                op,
+                code: code.clone(),
+            };
             let serialized = serde_json::to_string(&message).unwrap();
 
             exchange.publish(Publish::new(serialized.as_bytes(), "music_player"))?;
         } else {
             println!("Enter the music player auth code:");
-            buf.clear();
-            io::stdin().read_line(&mut buf)?;
-            buf = buf.trim().to_string();
 
-            if buf.len() != 6 {
-                println!("Please enter a 6-digit code!");
+            let new_code = read_line()?;
+            if new_code.len() != CODE_LENGTH {
+                println!("Please enter a {CODE_LENGTH}-digit code!");
                 continue;
             } else {
-                code = buf.clone();
+                code = new_code;
             }
         }
     }
