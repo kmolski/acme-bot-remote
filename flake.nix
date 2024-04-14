490c5b43b877b178dbdf2f96c36b4972ac99f762
kmolski <krzysztof.molski29@gmail.com>
2024-04-14 14:34:40 +0200

    build(nix): Add Nix flake for development shell

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+    };
+  };
+  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          overlays = [ (import rust-overlay) ];
+          pkgs = import nixpkgs { inherit system overlays; };
+          rustToolchain = pkgs.pkgsBuildHost.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+        in
+        {
+          devShells.default = with pkgs; mkShell {
+            buildInputs = [
+              rustToolchain
+              nodejs
+              trunk
+            ];
+          };
+        }
+      );
+}
