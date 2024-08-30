f4998f914c73bde354c8234ba6326133bfbdfe61
kmolski <krzysztof.molski29@gmail.com>
2024-08-30 20:00:24 +0200

    feat(ci): Add action to update flake.lock

--- flake.nix
+++ flake.nix
@@ -5,7 +5,6 @@
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
-      inputs.flake-utils.follows = "flake-utils";
     };
   };
   outputs = { self, nixpkgs, flake-utils, rust-overlay }:
